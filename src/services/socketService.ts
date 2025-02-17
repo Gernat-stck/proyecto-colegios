fc51f9dcbc157ee459efc0b5c3f8c712667d4b4f
Gernat <nagerman26@gmail.com>
2025-02-16 23:46:56 -0600

    Actualizar URL del microservicio en el servicio de socket

--- src/services/socketService.ts
+++ src/services/socketService.ts
@@ -1,9 +1,9 @@
 import { io, Socket } from "socket.io-client";
 
 let socket: Socket | null = null;
-
+const microServiceUrl = import.meta.env.VITE_MCS_URL;
 export const connectSocket = (token: string): Socket => {
-  socket = io('http://localhost:3000', {
+  socket = io(microServiceUrl, {
     auth: {
       token: token,
     },
