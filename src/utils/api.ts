d0c98613fd3a16fa1fc5083589278d273e899cfc
Gernat <nagerman26@gmail.com>
2025-02-16 22:01:38 -0600

    Agregar funciones para obtener conversaciones y mensajes desde la API

---
+++ src/utils/api.ts
@@ -0,0 +1,29 @@
+// utils/api.ts
+import axios from "axios";
+import { Conversation, Message } from "@/types/ChatType.d";
+
+const API_URL = import.meta.env.VITE_MCS_URL as string;
+const token = localStorage.getItem("microserviceToken"); // O donde almacenes el token
+
+export const fetchConversations = async (): Promise<Conversation[]> => {
+  const response = await axios.get(`${API_URL}conversations`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return response.data;
+};
+export const fetchMessages = async (
+  conversationId: string,
+  type: string
+): Promise<Message[]> => {
+  const response = await axios.get(
+    `${API_URL}messages/${type}/${conversationId}`,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+  return response.data;
+};
